3c2a9b5c993ea3911ed18e3d024ee01cac22089f
oculusK <kay5990@naver.com>
2023-10-01 16:44:45 +0900

    10월 1일 확인중

--- python/scapy_all.py
+++ python/scapy_all.py
@@ -1,10 +1,10 @@
 import sys
 from scapy.all import *
 
-print(conf.iface)
+#print(conf.ifaces)
 
 # 패킷 캡쳐를 시작합니다.
 # 인터페이스 이름을 실제로 사용하는 네트워크 인터페이스 이름으로 변경하세요.
 while True:
     sniff(iface="Software Loopback Interface 1", prn= lambda x : x.show())
-    #sniff(prn=lambda x: x.show())
+    #sniff(prn=lambda x:x.show())
