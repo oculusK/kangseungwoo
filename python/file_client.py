a44077361feec2df4971ee2d84a05aca9940eff7
oculusK <kay5990@naver.com>
2023-10-06 09:31:30 +0900

    10월 1일 확인중

--- python/file_client.py
+++ python/file_client.py
@@ -1,11 +1,15 @@
 import socket
+# client socket 생성
+
 s_sock = socket.socket()
 host = "localhost"
 port = 2600
 
-s_sock.connect((host,port))
-s_sock.send("I am ready".encode())
+s_sock.connect((host, port))
 
+# 서버에 I am ready 보내기
+s_sock.send("I am ready".encode())
+# 서버로부터 이름 수신
 fn = s_sock.recv(1024).decode()  ## 받았지만 차이를 위해 새로 지정
 
 with open("./dummy/"+"recv",'wb') as f:
--- python/file_server.py
+++ python/file_server.py
@@ -3,21 +3,29 @@ import sys
 
 port = 2600
 
+# 서보 소켓 생성
 s_sock = socket.socket()
-host = ''
-s_sock.bind((host,port))
-s_sock.listen(1)
+host = '' # 모든 IP로 접속을 받겠다.
+s_sock.bind((host, port))
+s_sock.listen(1) # 한 소켓으로만 받기위해서 1
 
 print("Waiting for Connection")
 
+# 클라이언트 연결대기
 c_sock, addr = s_sock.accept()
 print("connection from", addr)
+
+# 클라이언트로부터 메시지 수신및 출력
 msg = c_sock.recv(1024)
 print(msg.decode())
+
+# 파일 이름 입력받기
 filename = input("파일 이름")
 
+# 클라이언트에게 파일 이름 전송
 c_sock.send(filename.encode())
 
+# 파일 열어서 전송
 with open("./dummy/"+filename, 'rb') as f:
     c_sock.sendfile(f,0)
 
