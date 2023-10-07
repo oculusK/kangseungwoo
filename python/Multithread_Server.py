452476184d592d88530f018ec80dce81b0f47f3f
oculusK <kay5990@naver.com>
2023-10-07 21:58:26 +0900

    10월 7일 수업 내용

--- python/Multithread_Server.py
+++ python/Multithread_Server.py
@@ -8,15 +8,15 @@ BUFF = 1024
 def response(key):
     return '서버 응답 메시지'
 
-def handler(clientsock, addr): # 핸들러 함수
+def handler(clientsock, addr): #핸들러 함수
     while True:
         data = clientsock.recv(BUFF)
         print('data:' + repr(data))
         if not data: break
         clientsock.send(response('').encode())
-        print('send:' + repr(response('')))
+        print('sent:' + repr(response('')))
 
-if __name__ == '__main__':
+if __name__=='__main__':
     ADDR = (HOST, PORT)
     serversock = socket(AF_INET, SOCK_STREAM)
     serversock.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     serversock.listen(5)
 
 while True:
-    print('waiting fir connection...')
-    clientsock, addr = serversock.accept()
-    print('...connected from:', addr)
-    _thread.start_new_thread(handler, (clientsock, addr))
+        print('waiting for connection...')
+        clientsock, addr = serversock.accept()
+        print('...connected from:', addr)
+        _thread.start_new_thread(handler, (clientsock, addr))
--- python/TCP_Echo_Client_Program.py
+++ python/TCP_Echo_Client_Program.py
@@ -1,11 +1,22 @@
 import socket
-port = int(input("Port No: "))
-address = ("localhost", port)
+#port = int(input("Port No: "))
+PORT = 2500
+HOST = 'localhost'
+#address = ("localhost", port)
+address = (HOST, PORT) # 주소는 항상 (ip, port) 튜플
 BUFSIZE = 1024
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(address)
+s.connect(address) # 서버 연결 요청
 while True:
     msg = input("Message to send: ")
-    s.send(msg.encode())
-    data = s.recv(BUFSIZE)
-    print("Received message: %s" %data.decode())
+
+    if not msg:
+        continue
+    try:
+        s.send(msg.encode()) # 메시지 서버로 보내기
+    except:
+        print("연결 종료")
+        break
+
+    #data = s.recv(BUFSIZE)
+    #print("Received message: %s" %data.decode())
