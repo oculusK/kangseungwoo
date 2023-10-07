b44986105dc03b4a416926a308f2c5f63b7820ca
oculusK <kay5990@naver.com>
2023-10-07 21:49:27 +0900

    10월 7일 수업 내용

--- python/video/video_client.py
+++ python/video/video_client.py
@@ -7,7 +7,7 @@ import struct
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 host_ip = 'localhost' # 서버 IP 주소
 port = 9100
-client_socket.connect((host_ip,port)) # 서버와 연결
+client_socket.connect((host_ip, port)) # 서버와 연결
 data = b""
 payload_size = struct.calcsize("Q") # 데이터는 unsigned Long Long
 
@@ -20,15 +20,15 @@ while True:
             data += packet
         packed_msg_size = data[:payload_size] # 프레임 길이 추출
         data = data[payload_size:] # 프레임 추출
-        msg_size = struct.unpack("Q",packed_msg_size)[0] # 프레임 길이를 파이썬 형태로 변환
+        msg_size = struct.unpack("Q", packed_msg_size)[0] # 프레임 길이를 파이썬 형태로 변환
 
         while len(data) < msg_size: # 길이 만큼 프레임 수신
             data += client_socket.recv(4*1024)
         frame_data = data[:msg_size] # 한 프레임 만큼 얻음
         data = data[msg_size:] # 다음
 
-        frame  = pickle.loads(frame_data) # 바이트 스트림을 프레임으로 변환
-        cv2.imshow("c",frame)
+        frame = pickle.loads(frame_data) # 바이트 스트림을 프레임으로 변환
+        cv2.imshow("c", frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
--- python/video/video_server.py
+++ python/video/video_server.py
@@ -12,24 +12,24 @@ server_addr = ('',port)
 server_socket.bind(server_addr)
 # 접속대기
 server_socket.listen(5)
-print("접속대기",server_addr)
+print("접속대기", server_addr)
 # 클라이언트 연결
 while True:
     client_socket, addr = server_socket.accept()
-    print(addr,'와 연결됨')
+    print(addr, '와 연결됨')
     if client_socket:
         vid = cv2.VideoCapture(0) # 웹캠 연결
         if vid.isOpened():
-            print(vid.get(3),vid.get(4))
+            print(vid.get(3), vid.get(4))
         while vid.isOpened():
             img, frame = vid.read() # 프레임 획득
-            frame = imutils.resize(frame,width=640) # 프레임 크기 조절
+            frame = imutils.resize(frame, width=640) # 프레임 크기 조절
             frame_bytes = pickle.dumps(frame) # 프레임을 바이트 스트림으로 변환
             msg = struct.pack("Q",len(frame_bytes)) + frame_bytes
             # 메시지 Q unsigned long long d으로 보낼 데이터 크기 전송
             client_socket.sendall(msg)
 
-            cv2.imshow('s',frame)
+            cv2.imshow('s', frame)
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
                 client_socket.close()
