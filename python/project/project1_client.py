5194edef78ad723b0c87c589130d693d4a2c513f
oculusK <kay5990@naver.com>
2023-10-08 19:25:18 +0900

    비디오 자체가 안되는중...

--- python/project/project1_client.py
+++ python/project/project1_client.py
@@ -1,48 +1,36 @@
-from tkinter import *
-from socket import *
-import threading
+# -*- coding: utf8 -*-
 import cv2
-import pickle
-import struct
+import socket
+import numpy as np
 
-# 섭씨 온도를 서버로 전송
-def calculate():
-    global temp
-    temp = float(entry1.get()) # temp 읽기
-    sock.send(str(temp).encode()) # send the temp in C to Server
+## TCP 사용
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+## server ip, port
+s.connect(('192.168.1.83', 8485))
 
-def handler(sock):
-    while True:
-        try: # 데이터 수신 예외처리
-            r_msg = sock.recv(1024)
-        except:
-            pass
-        else : # 수신 데이터 표시
-            entry2.delete(0, END)
-            entry2.insert(0, r_msg.decode())
+## webcam 이미지 capture
+cam = cv2.VideoCapture(0)
 
-sock = socket(AF_INET, SOCK_STREAM)
-sock.connect(("localhost", 2500))
+## 이미지 속성 변경 3 = width, 4 = height
+cam.set(3, 320);
+cam.set(4, 240);
 
-payload_size = struct.calcsize("Q") # 데이터는 unsigned Long Long
+## 0~100에서 90의 이미지 품질로 설정 (default = 95)
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
-root = Tk()
-message_label = Label(text='Enter a temperature(C) ', font=('Verdana', 16))
-entry1 = Entry(font=('Verdana', 16), width=5)
+while True:
+    # 비디오의 한 프레임씩 읽는다.
+    # 제대로 읽으면 ret = True, 실패면 ret = False, frame에는 읽은 프레임
+    ret, frame = cam.read()
+    # cv2. imencode(ext, img [, params])
+    # encode_param의 형식으로 frame을 jpg로 이미지를 인코딩한다.
+    result, frame = cv2.imencode('.jpg', frame, encode_param)
+    # frame을 String 형태로 변환
+    data = np.array(frame)
+    stringData = data.tostring()
 
-recv_label = Label(text='Temperature in F ', font=('Verdana', 16))
-entry2 = Entry(font=('Verdana', 16), width=5)
+    # 서버에 데이터 전송
+    # (str(len(stringData))).encode().ljust(16)
+    s.sendall((str(len(stringData))).encode().ljust(16) + stringData)
 
-calc_button = Button(text='전송', font=('Verdana', 12), command=calculate)
-message_label.grid(row=0, column=0, sticky=W)
-recv_label.grid(row=1, column=0, sticky=W)
-entry1.grid(row=0, column=1)
-entry2.grid(row=1, column=1)
-calc_button.grid(row=0, column=2, padx=10, pady=10)
-
-# 데이터 수신을 위한 thread 생성과 실행
-cTread = threading.Thread(target=handler, args=(sock,))
-cTread.daemon = True
-cTread.start()
-
-root.mainloop()
+cam.release()
