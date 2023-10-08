e3e893bf719ed6db356ed153b6cee7fce96be559
oculusK <kay5990@naver.com>
2023-10-08 20:23:01 +0900

    10월 8일 과제 수행 중 통신 이해하기

--- python/project/socket_server.py
+++ python/project/socket_server.py
@@ -1,6 +1,21 @@
 from socket import *
+import threading
+import time
 
-port = 8080
+
+def send(sock):
+    while True:
+        sendData = input('>>>')
+        sock.send(sendData.encode('utf-8'))
+
+
+def receive(sock):
+    while True:
+        recvData = sock.recv(1024)
+        print('상대방 :', recvData.decode('utf-8'))
+
+
+port = 8081
 
 serverSock = socket(AF_INET, SOCK_STREAM)
 serverSock.bind(('', port))
@@ -12,9 +27,12 @@ connectionSock, addr = serverSock.accept()
 
 print(str(addr), '에서 접속되었습니다.')
 
-while True:
-    sendData = input('>>>')
-    connectionSock.send(sendData.encode('utf-8'))
+sender = threading.Thread(target=send, args=(connectionSock,))
+receiver = threading.Thread(target=receive, args=(connectionSock,))
 
-    recvData = connectionSock.recv(1024)
-    print('상대방 :', recvData.decode('utf-8'))
+sender.start()
+receiver.start()
+
+while True:
+    time.sleep(1)
+    pass
