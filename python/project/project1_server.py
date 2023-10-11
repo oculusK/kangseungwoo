24f3f04c0e1cca6b3a07004b2a878144bce5a221
oculusK <kay5990@naver.com>
2023-10-11 22:22:31 +0900

    10월 11일 조정중

--- python/project/project1_server.py
+++ python/project/project1_server.py
@@ -1,6 +1,7 @@
 import socket
 import cv2
 import numpy as np
+import select
 
 # socket에서 수신한 버퍼를 반환하는 함수
 def recvall(sock, count):
@@ -33,27 +34,34 @@ def handler(s): # 클라이언트와 연결을 관리하는 함수(핸들러 함
 # 서버 설정
 HOST = ''
 PORT = 8080
+BUFFER = 1024
 
 # 클라이언트 저장을 위한 리스트
 clients = []
 
+# 소켓 저장을 위한 리스트
+sock_list = []
+
 # TCP 사용
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print('Socket created')
+s_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
+#print('Socket created')
 # 서버의 아이피와 포트번호 지정
-s.bind((HOST, PORT))
-print('Socket bind complete')
+s_sock.bind((HOST, PORT))
+#print('Socket bind complete')
 # 클라이언트의 접속을 기다린다. (클라이언트 연결을 10개까지 받는다)
-s.listen(10)
-print('Socket now listening')
+s_sock.listen(10)
+#print('Socket now listening')
+
+sock_list.append(s_sock)
 
 # 연결, conn에는 소켓 객체, addr은 소켓에 바인드 된 주소
-conn, addr = s.accept()
+conn, addr = s_sock.accept()
 
 while True:
-    # client에서 받은 stringData의 크기 (==(str(len(stringData))).encode().ljust(16))
+    # client에서 받은 stringData의 크기
     length = recvall(conn, 16)
     stringData = recvall(conn, int(length))
     data = np.frombuffer(stringData, dtype='uint8')
@@ -65,7 +73,28 @@ while True:
 
 # 클라이언트 연결 수락 및 스레드 생성
 while True:
-    client_socket, addr = s.accept()
+    client_socket, addr = s_sock.accept()
     clients.append(client_socket)
     client_thread = threading.Thread(target=handler, args=(client_socket,))
     client_thread.start()
+
+while True:
+    r_sock, w_sock, e_sock = select.select(sock_list, [], []) # 읽기 이벤트 조사
+    print(r_sock)
+    for s in r_sock:
+        if s == s_sock: # 서버에서 연결 이벤트 발생
+            c_sock, addr = s_sock.accept()
+            sock_list.append(c_sock) # 새로운 Client 소켓 목록 추가
+            print(" Client (%s, %s) connected" % addr)
+        else: # 데이터 도착 이벤트
+            try:
+                data = s.recv(BUFFER) # Client에서 읽기 이벤트
+                print("Received: ", data.decode())
+                if data:
+                    s.send(data)
+            except:
+                print("Client (%s, %s) is offline" % addr)
+                s.close()
+                sock_list.remove(s) # 연결 종료된 Client 제거
+        continue
+s_sock.close()
